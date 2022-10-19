aa8a90870d1d62258364a38bc764ce4da93280da
lea <leampdi@gmail.com>
2022-10-19 13:44:25 +0300

    daily Week2 day2

---
+++ Week2/Week2Day2/daily/scripts.js
@@ -0,0 +1,42 @@
+// Instructions
+
+//     Create a variable called sentence. The value of the variable should be a string that contains the words “not” and “bad”.
+//     For example, “The movie is not that bad, I like it”.
+
+//     Create a variable called wordNot where it’s value is the first appearance (ie. the position) of the substring “not” (from the sentence variable).
+
+//     Create a variable called wordBad where it’s value is the first appearance (ie. the position) of the substring “bad” (from the sentence variable).
+
+//     If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
+//     For example, the result here should be : “The movie is good, I like it”
+//     If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
+
+// Example:
+
+//   Your string is : 'This dinner is not that bad ! You cook well', 
+//   --> the result is : 'This dinner is good ! You cook well'
+
+//   Your string is : 'This movie is not so bad !' 
+//   --> the result is : 'This movie is good !'
+
+//   Your string is : 'This dinner is bad !' 
+//   --> the result is : 'This dinner is bad !'
+
+
+
+let sentence = "peopple are generaly not bad";
+
+let splitSentenc = sentence.split(' ')
+console.log(splitSentenc)
+
+let wordNot = splitSentenc.indexOf("not")
+let wordBad = splitSentenc.indexOf("bad")
+
+console.log(wordNot, wordBad)
+
+if(wordNot+1 == wordBad){
+    splitSentenc.splice(wordNot, 2 ,"very good people")
+    console.log(splitSentenc.join().replace(',', ' '))
+} else if ( wordNot+1 !== wordBad || !wordNot ){
+    console.log(sentence)
+}
