2cbc4046b83c36e6621ada9810b6294a8076f3d6
lea <leampdi@gmail.com>
2022-10-19 10:13:24 +0300

    fix

--- Week2/Week2Day1/daily/script.js
+++ Week2/Week2Day1/daily/script.js
@@ -40,4 +40,4 @@ const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
     // Access and then console.log “Oranges”.
 
 
-console.log("more fuits", moreFruits[1][1])
+console.log("more fuits", moreFruits[1][1][0])
