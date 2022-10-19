a4b39602ff244046f28ec745a0100258ef14f830
lea <leampdi@gmail.com>
2022-10-19 09:11:22 +0300

    intro to js

---
+++ Week2/Week2Day1/intro_js/scripts.js
@@ -0,0 +1,47 @@
+let username = "yo"
+
+console.log("The name of the user is ", username)
+
+
+let bestway = `the user here is ${username}`
+
+console.log(bestway)
+
+
+
+
+
+// Class Ex
+addressNumber = "5"
+addressStreet = "Abarbanel Street"
+country = "Israel"
+
+globalAddress = `I live in ${addressStreet} ${addressNumber}, whici is in ${country}`
+
+
+
+
+// tore your birth year in a variable.
+
+// 2. Store a future year in a variable.
+
+// 3. Calculate your possible ages for that year based on the stored values.
+
+// 4. Display : "I will be NN in YYYY", substituting the values. 
+
+let dob = 1986
+let futur = 2050
+
+let myFuturAge = futur - dob
+
+console.log(myFuturAge)
+
+
+// to get the answer from propmp
+
+const userAnswer = prompt("what's your favorit pet")
+console.log(userAnswer)
+
+
+
+console.log(country.indexOf("l"))
