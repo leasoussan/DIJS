8a3adc55a08f139b2d80092ad88ba5e2819154f5
lea <leampdi@gmail.com>
2022-11-22 15:35:14 +0200

    xp 1 ok

--- Week7/Week7Day2/node-server/myserver.js
+++ Week7/Week7Day2/node-server/myserver.js
@@ -1,6 +1,4 @@
-const http = require('http')
 const express = require('express')
-const {user} = require('./user.js')
 
 const app = express() 
 
@@ -11,9 +9,12 @@ app.listen(3000, () => {
 // app.use('/', express.static(__dirname))
 
 app.get('/', (req, res) => {
-    res.send('<h1>HERE IS the first line</h1>')
-    res.write('<h3>HERE IS the second line</h3>')
-    res.end('<p>HERE IS the third line</p>')
+    
+    res.write('<h1>HERE IS the first line</h1>');
+    res.write('<h3>HERE IS the second line</h3>');
+    res.write('<p>HERE IS the paragraph line</h3>');
+          
+    res.end()
 }
 )
 
